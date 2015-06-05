5b5c570dfcd60f156734440c6bd0035ea8216515
Rick Almeida <Rick.Almeida@smartbear.com>
2015-06-05 14:40:47 -0400

    sample commit

--- Main.java
+++ Main.java
@@ -2,6 +2,29 @@ package com.smartbear.demo;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String args[]) {
+
+	    //input to print Fibonacci series upto how many numbers
+        System.out.print("Enter number up to which Fibonacci series to print: ");
+        int number = new Scanner(System.in).nextInt();
+
+        System.out.println("\n\nFibonacci series up to " + number +" numbers : ");
+        //printing Fibonacci series up to number
+        for(int i=1; i<=number; i++){
+            System.out.println(fibonacciLoop(i));
+        }
+    }
+
+
+    // Java program for Fibonacci number using recursion.
+    public static long fibonacciRecusion(long number){
+        if(number == 1 || number == 2){
+            return 1;
+        }
+
+        return fibonacciRecusion(number-1) + fibonacciRecusion(number -2); //tail recursion
+    }
+
     // Java program for Fibonacci number using Loop.
     public static long fibonacciLoop(int number){
         if(number == 1 || number == 2){
