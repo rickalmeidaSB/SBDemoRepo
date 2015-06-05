920a7ccbd93b83abd84b1d7ff4336dffc4616c3e
Rick Almeida <Rick.Almeida@smartbear.com>
2015-06-05 14:18:03 -0400

    feature 1

--- Main.java
+++ Main.java
@@ -2,5 +2,18 @@ package com.smartbear.demo;
 import java.util.Scanner;
 
 public class Main {
+    // Java program for Fibonacci number using Loop.
+    public static long fibonacciLoop(int number){
+        if(number == 1 || number == 2){
+            return 1;
+        }
+        long fibo1=1, fibo2=1, fibonacci=1;
+        for(int i= 3; i<= number; i++){
+            fibonacci = fibo1 + fibo2; //Fibonacci number is sum of previous two Fibonacci number
+            fibo1 = fibo2;
+            fibo2 = fibonacci;
 
+        }
+        return fibonacci; //Fibonacci number
+    }
 }
